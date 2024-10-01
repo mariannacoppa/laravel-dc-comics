6dc75cee3b1d07b8a3f8983ef135959d683b1bff
mariannacoppa <densimetra@gmail.com>
2024-10-01 19:44:18 +0200

    [UPD] created routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController as ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('homepage');
+
+Route::resource('/comics', ComicController::class);
