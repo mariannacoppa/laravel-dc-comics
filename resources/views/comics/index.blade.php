f853429356f98468b5e1acc77a950468e236cfb8
mariannacoppa <densimetra@gmail.com>
2024-10-01 19:48:04 +0200

    [UPD] created index.blade.php

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,30 @@
+@section('content')
+<main>
+    <div id="jumbotron">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="col-12">
+                        <div class="badge p-3 bg-primary text-uppercase">
+                            current series
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="comics-container">
+        <div class="row">
+            <div class="col-12">
+
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="content d-flex justify-content-center">
+
+                <div class="btn btn-primary my-3 text-uppercase">load more</div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
