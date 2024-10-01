b0d8c29bb1f6040be18bc9a677eb630ec11e028a
mariannacoppa <densimetra@gmail.com>
2024-10-01 19:17:50 +0200

    [UPD] imported model, created new instance for each comic in seeder file

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// importo il model
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +16,27 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // recupero il contenuto del file comics.php nella cartella config
+        $comics = config('comics');
+        foreach($comics as $comic){
+            // creo una nuova istanza per ogni fumetto
+            $new_comic = new Comic();
+
+            $new_comic->title = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            // creo una stringa col metodo implode
+            $artists_string = implode(', ', $comic['artists']);
+            $writers_string = implode(', ', $comic['writers']);
+            $new_comic->artists = $artists_string;
+            $new_comic->writers = $writers_string;
+
+            $new_comic->save();
+
+            }
+        
     }
 }
