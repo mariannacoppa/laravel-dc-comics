8864a183ed5da0295f32862280fbb6ca3857ee17
mariannacoppa <densimetra@gmail.com>
2024-10-01 19:58:50 +0200

    [UPD] in controller file got comics, brought them to the view through compact method and showed them in index file

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -16,7 +16,14 @@
     <div id="comics-container">
         <div class="row">
             <div class="col-12">
-
+                @foreach ($comics as $comic)
+                <div class="comic">
+                    <div class="thumb">
+                        <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                    </div>
+                    <h4 class="text-uppercase">{{$comic['title']}}</h4>
+                </div>
+                @endforeach
             </div>
         </div>
         <div class="col-12">
