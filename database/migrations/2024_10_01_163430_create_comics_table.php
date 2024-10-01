62535448e9fc7bc1ea042cfc6bd0972614d87f3b
mariannacoppa <densimetra@gmail.com>
2024-10-01 18:43:33 +0200

    [UPD] done migration

--- database/migrations/2024_10_01_163430_create_comics_table.php
+++ database/migrations/2024_10_01_163430_create_comics_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->decimal('price', 5, 2);
+            $table->date('sale_date');
+            $table->string('type');
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
             $table->timestamps();
         });
     }
