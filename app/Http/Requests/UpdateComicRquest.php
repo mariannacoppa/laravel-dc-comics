13677389e6717f99740822cf8f41f43448c16b52
mariannacoppa <densimetra@gmail.com>
2024-10-05 20:12:25 +0200

    [UPD] set the same tules in update request file

--- app/Http/Requests/UpdateComicRquest.php
+++ app/Http/Requests/UpdateComicRquest.php
@@ -24,7 +24,25 @@ class UpdateComicRquest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|max:255',
+            'thumb' => 'max:255',
+            'price' => 'required|max:20',
+            'sale_date' => 'required|date',
+            'type' => 'required|max:255'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'title.required' => 'il titolo è obbligatorio',
+            'title.max' => 'il titolo può essere lungo al massimo :max caratteri',
+            'thumb.max' => 'il link all\'immagine può essere lungo al massimo :max caratteri', 
+            'price.required' => 'il prezzo è obbligatorio',
+            'price.max' => 'il prezzo può essere lungo al massimo :max caratteri',
+            'sale_date.required' => 'la data è obbligatoria',
+            'sale_date.date' => 'la data dev\'essere espressa in un formato valido',
+            'type.required' => 'il tipo è obbligatorio',
+            'type.max' => 'il tipo può essere lungo al massimo :max caratteri',
         ];
     }
 }
